5b9e05fc5b6bee650bfcbeb2498ce70c6202bf64
Takashi Matsuoka <matsujirushi@live.jp>
2021-11-10 10:27:08 +0900

    Fix #8

--- app/src/main/java/cc/seeed/iot/activity/HelpActivity.java
+++ app/src/main/java/cc/seeed/iot/activity/HelpActivity.java
@@ -44,46 +44,12 @@ public class HelpActivity extends BaseActivity {
     }
 
     private void initDate() {
-        int url = R.mipmap.help01_breathing;
-        String ques = "Setup mode";
-        String answer = "When blue LED is breathing, your Wio device is under Setup mode and ready to be connected. \r\n" +
-                "To set your WIO device to Setup mode, please press and hold Config button for about 4 second for WIO Link, Function button for WIO Node.";
-        FAQBean bean1 = new FAQBean(url, ques, answer);
-
-        url = R.mipmap.help02_blink_2;
-        ques = "Requesting IP address from router";
-        answer = "When blue LED blinks twice then quickly off for 1 second, your Wio device is requesting IP address from router. \r\n" +
-                "It might take up to a minute.";
-        FAQBean bean2 = new FAQBean(url, ques, answer);
-
-        url = R.mipmap.help03_blink_1_quick;
-        ques = "Connecting to server";
-        answer = "When blue LED blinks once then quickly off for 1 second, your Wio device is connecting to server. It might take up to a minute.";
-        FAQBean bean3 = new FAQBean(url, ques, answer);
-
-        url = R.mipmap.help04_no_blink;
-        ques = "Not getting IP / fail connecting to server";
-        answer = "When blue LED is constantly on, indicates your Wio device is not getting IP / fail connecting to server. \r\n" +
-                "Please check your internet connection and try again from the start.";
-        FAQBean bean4 = new FAQBean(url, ques, answer);
-
-        url = R.mipmap.help05_blink_01s;
-        ques = "OTA";
-        answer = "When blue LED is blinking constantly, your Wio device is updating firware (OTA).\r\n" +
-                "It might take up to a minute.";
-        FAQBean bean5 = new FAQBean(url, ques, answer);
-
-        url = R.mipmap.help06_blink_1s;
-        ques = "Device online";
-        answer = "When blue LED is on for 1 second then off for another, your Wio device is online.";
-        FAQBean bean6 = new FAQBean(url, ques, answer);
-
-        beanList.add(bean1);
-        beanList.add(bean2);
-        beanList.add(bean3);
-        beanList.add(bean4);
-        beanList.add(bean5);
-        beanList.add(bean6);
+        beanList.add(new FAQBean(R.mipmap.help01_breathing, getString(R.string.help_bean1_title), getString(R.string.help_bean1_msg)));
+        beanList.add(new FAQBean(R.mipmap.help02_blink_2, getString(R.string.help_bean2_title), getString(R.string.help_bean2_msg)));
+        beanList.add(new FAQBean(R.mipmap.help03_blink_1_quick, getString(R.string.help_bean3_title), getString(R.string.help_bean3_msg)));
+        beanList.add(new FAQBean(R.mipmap.help04_no_blink, getString(R.string.help_bean4_title), getString(R.string.help_bean4_msg)));
+        beanList.add(new FAQBean(R.mipmap.help05_blink_01s, getString(R.string.help_bean5_title), getString(R.string.help_bean5_msg)));
+        beanList.add(new FAQBean(R.mipmap.help06_blink_1s, getString(R.string.help_bean6_title), getString(R.string.help_bean6_msg)));
 
         HelpAdapter adapter = new HelpAdapter( beanList);
         View headerView = LayoutInflater.from(this).inflate(R.layout.help_header_layout, null);
